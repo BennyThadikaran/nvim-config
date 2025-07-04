1c17b1a4d89b3ce2d58578d933388cf56944ccf8
Benny Thadikaran <tbenny05@yahoo.com>
2025-07-04 16:19:53 +0530

    remove some unused keymaps

--- lua/dev/core/keymaps.lua
+++ lua/dev/core/keymaps.lua
@@ -43,25 +43,3 @@ vim.api.nvim_create_autocmd("FileType", {
 
 -- clear search highlights
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
-
--- increment/decrement numbers
-keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
-keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
-
--- window management
-keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
-keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
-keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
-keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
-
--- Tabs
-keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
-keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
-keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
-keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
-keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
-
--- buffer
-keymap.set("n", "<Tab>", ":bnext <CR>", { desc = "Next Buffer" })
-keymap.set("n", "<S-Tab>", ":bprevious <CR>", { desc = "Previous Buffer" })
-keymap.set("n", "<leader>d", ":bd! <CR>", { desc = "Delete Buffer" })

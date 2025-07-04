449990e63f0e3d94264e8d40c2bb6c1fd55fab50
Benny Thadikaran <tbenny05@yahoo.com>
2025-07-04 16:22:37 +0530

    shorten vim.keymap.set in keymaps.lua

--- lua/dev/core/keymaps.lua
+++ lua/dev/core/keymaps.lua
@@ -1,45 +1,44 @@
 -- set leader key to space
 vim.g.mapleader = ","
 
-local keymap = vim.keymap -- for conciseness
+local map = vim.keymap.set -- for conciseness
 
 ---------------------
 -- General Keymaps -------------------
-keymap.set("n", "<leader>w", ":w!<CR>", { desc = "Quick save file" })
-keymap.set("n", "<leader>cx", "<cmd>silent !chmod +x %<CR>", { desc = "Make file executable", noremap = true })
-keymap.set("n", "<leader>cd", "<cmd>lcd %:p:h<cr><cmd>pwd<cr>", { desc = "Change folder to current file folder" })
+map("n", "<leader>w", ":w!<CR>", { desc = "Quick save file" })
+map("n", "<leader>cd", "<cmd>lcd %:p:h<cr><cmd>pwd<cr>", { desc = "Change folder to current file folder" })
 
-keymap.set("v", "<", "<gv", { desc = "Indent line" })
-keymap.set("v", ">", ">gv", { desc = "Indent line" })
+map("v", "<", "<gv", { desc = "Indent line" })
+map("v", ">", ">gv", { desc = "Indent line" })
 
 -- Execute file based on filetype
 vim.api.nvim_create_autocmd("FileType", {
 	pattern = "python",
-	callback = function(args)
-		vim.keymap.set("n", "<leader>e", ":w<CR>:exec '!python3' shellescape(@%, 1)<CR>")
+	callback = function(_)
+		map("n", "<leader>e", ":w<CR>:exec '!python3' shellescape(@%, 1)<CR>")
 	end,
 })
 
 vim.api.nvim_create_autocmd("FileType", {
 	pattern = "javascript",
-	callback = function(args)
-		vim.keymap.set("n", "<leader>e", ":w<CR>:exec '!node' shellescape(@%, 1)<CR>")
+	callback = function(_)
+		map("n", "<leader>e", ":w<CR>:exec '!node' shellescape(@%, 1)<CR>")
 	end,
 })
 
 vim.api.nvim_create_autocmd("FileType", {
 	pattern = "java",
-	callback = function(args)
-		vim.keymap.set("n", "<leader>e", ":w<CR>:exec '!java' shellescape(@%, 1)<CR>")
+	callback = function(_)
+		map("n", "<leader>e", ":w<CR>:exec '!java' shellescape(@%, 1)<CR>")
 	end,
 })
 
 vim.api.nvim_create_autocmd("FileType", {
 	pattern = "c",
-	callback = function(args)
-		vim.keymap.set("n", "<leader>e", ":w<CR>:!gcc % -o %:r -O -W -Wall -pedantic -ansi -lm -std=c99 && ./%:r<CR>")
+	callback = function(_)
+		map("n", "<leader>e", ":w<CR>:!gcc % -o %:r -O -W -Wall -pedantic -ansi -lm -std=c99 && ./%:r<CR>")
 	end,
 })
 
 -- clear search highlights
-keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
+map("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })

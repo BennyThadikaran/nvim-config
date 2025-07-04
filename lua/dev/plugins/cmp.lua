f8656420b4585c502a569a41b9dcbee500bcd126
Benny Thadikaran <tbenny05@yahoo.com>
2025-07-04 13:29:47 +0530

    Remove default config for blink.nvim

--- lua/dev/plugins/cmp.lua
+++ lua/dev/plugins/cmp.lua
@@ -31,12 +31,5 @@ return {
 				"fallback",
 			},
 		},
-		appearance = {
-			nerd_font_variant = "mono",
-		},
-
-		completion = { documentation = { auto_show = false } },
-
-		fuzzy = { implementation = "prefer_rust_with_warning" },
 	},
 }
